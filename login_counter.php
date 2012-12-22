597cc4a5778537c6524f3eca02d9422bbbc1bef2
Derek Hall <einhander.iniq@gmail.com>
2012-12-22 10:44:25 -0500

    Add the initial sketch of a plugin script
    Just includes the description and copyright at the moment.

---
+++ login_counter.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Wordpress Login Counter
+Plugin URI: https://github.com/iniq/WP-Login-Counter
+Description: Add columns to the Users list indicating number of times logged in and when the last login occurred.
+Version: 1.0
+Author: Derek Hall
+Author URI: http://iniquitous.ca
+*/
+
+/*
+Copyright 2012  Derek Hall (dhall@iniquitous.ca)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+*/
+
